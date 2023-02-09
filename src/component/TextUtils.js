d1cf84b1a89d9213f3b6058cf6bb182e280c1037
mitulsavani100 <mitulsavani100@gmail.com>
2023-02-09 17:21:09 +0530

    other changes

--- src/component/TextUtils.js
+++ src/component/TextUtils.js
@@ -21,13 +21,28 @@ const TextUtils = (props) => {
     let splitString = text.replace(/\s/g, '');
     setText(splitString);
   }
-  const clickSplitExtraspace = () =>{
+  const clickLine = () =>{
     const singleLineText = text
     .split("\n")
     .map(line => line.trim())
     .join(" ");
     // setText(text.replace(/\r?\n/g, ' ').trim());
     setText(singleLineText);
+  }
+
+  const clickSplitExtraspace = () =>{
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  }
+
+  const clickcopyText =() =>{
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+  const clickclearText =() =>{
+    let newText = '';
+    setText(newText);
   }
   const textChange = (e) =>{
     setText(e.target.value);
@@ -37,13 +52,16 @@ const TextUtils = (props) => {
     <>
       <div className='container mx-auto px-4'>
         <h1 className='text-center text-4xl font-bold my-5'>{props.heading}</h1>
-        <textarea className='w-full border-2 p-4 rounded-lg' value={text} name="" id="" cols="30" rows="10" onChange={textChange} placeholder='Enter Yout Utils text'></textarea>
+        <textarea className='w-full border-2 p-4 rounded-lg' value={text} name="" id="myBox" cols="30" rows="10" onChange={textChange} placeholder='Enter Yout Utils text'></textarea>
         <div className='flex space-x-4 flex-wrap mt-5'>
-          <button onClick={clickUppercase} className='capitalize bg-[#0C6FC0] border-2 border-[#0C6FC0] hover:bg-transparent hover:text-[#0C6FC0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>uppercase</button>
-          <button onClick={clickLowercase} className='capitalize bg-[#0C6FC0] border-2 border-[#0C6FC0] hover:bg-transparent hover:text-[#0C6FC0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>lowercase</button>
-          <button onClick={clickCamelcase} className='capitalize bg-[#0978D0] border-2 border-[#0978D0] hover:bg-transparent hover:text-[#0978D0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>camelcase</button>
-          <button onClick={clickSplit} className='capitalize bg-[#0978D0] border-2 border-[#0978D0] hover:bg-transparent hover:text-[#0978D0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>Split Text</button>
-          <button onClick={clickSplitExtraspace} className='capitalize bg-[#0978D0] border-2 border-[#0978D0] hover:bg-transparent hover:text-[#0978D0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>Split Extra space</button>
+          <button onClick={clickUppercase} className='capitalize bg-opacity-60 hover:bg-opacity-100 bg-[#0C6FC0] border-2 border-[#0C6FC0] hover:bg-transparent hover:text-[#0C6FC0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>uppercase</button>
+          <button onClick={clickLowercase} className='capitalize bg-opacity-60 hover:bg-opacity-100 bg-[#0C6FC0] border-2 border-[#0C6FC0] hover:bg-transparent hover:text-[#0C6FC0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>lowercase</button>
+          <button onClick={clickCamelcase} className='capitalize bg-opacity-60 hover:bg-opacity-100 bg-[#0978D0] border-2 border-[#0978D0] hover:bg-transparent hover:text-[#0978D0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>camelcase</button>
+          <button onClick={clickSplit} className='capitalize bg-opacity-60 hover:bg-opacity-100 bg-[#0978D0] border-2 border-[#0978D0] hover:bg-transparent hover:text-[#0978D0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>Split Text</button>
+          <button onClick={clickLine} className='capitalize bg-opacity-60 hover:bg-opacity-100 bg-[#0978D0] border-2 border-[#0978D0] hover:bg-transparent hover:text-[#0978D0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>Split Line</button>
+          <button onClick={clickSplitExtraspace} className='capitalize bg-opacity-60 hover:bg-opacity-100 bg-[#0978D0] border-2 border-[#0978D0] hover:bg-transparent hover:text-[#0978D0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>Split Extra space</button>
+          <button onClick={clickcopyText} className='capitalize bg-opacity-60 hover:bg-opacity-100 bg-[#0978D0] border-2 border-[#0978D0] hover:bg-transparent hover:text-[#0978D0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>Copy Text</button>
+          <button onClick={clickclearText} className='capitalize bg-opacity-60 hover:bg-opacity-100 bg-[#0978D0] border-2 border-[#0978D0] hover:bg-transparent hover:text-[#0978D0] font-semibold duration-300 px-8 py-3 text-white rounded-md'>Clear Text</button>
         </div>
         <p className='my-10'>Word: {text.split(" ").length-1}</p>
         <p className='my-10'>Character: {text.length} </p>
